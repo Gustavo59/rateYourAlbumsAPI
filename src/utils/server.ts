115f2e4c4a181281e42163fe6cd2f8eeb353b417
Gustavo59 <gustavosbarros59@gmail.com>
2021-09-26 16:25:57 -0300

    Primeira integração com o express

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,12 +1,46 @@
 import express from 'express';
+import * as OpenApiValidator from 'express-openapi-validator';
 import { Express } from 'express-serve-static-core';
+import { connector, summarise } from 'swagger-routes-express';
+import YAML from 'yamljs';
+
+import * as api from '../api/controllers';
+
 
 export async function createServer() : Promise<Express> {
+    const yamlSpecFile = './config/openapi.yml'
+    const apiDefinition = YAML.load(yamlSpecFile)
+    const apiSummary = summarise(apiDefinition)
+    console.info(apiSummary)
+
     const server = express()
 
-    server.get('/', (req, res) => {
-        res.send('Hello world!!!')
+    const validatorOptions = {
+        coerceTypes: true,
+        apiSpec: yamlSpecFile,
+        validateRequests: true,
+        validateResponses: true
+    }
+
+    server.use(OpenApiValidator.middleware(validatorOptions))
+
+    server.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+        res.status(err.status).json({
+            error: {
+                type: 'request_validation',
+                message: err.message,
+                errors: err.errors
+            }
+        })
     })
+
+    const connect = connector(api, apiDefinition, {
+        onCreateRoute: (method: string, descriptor: any[]) => {
+            console.log(`${method}: ${descriptor} : ${(descriptor[1] as any).name}`)
+        }
+    })
+
+    connect(server)
     
-    return server    
+    return server
 }
