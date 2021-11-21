d6f957d39ce904cd292cca8d06140894c9d3ac81
Gustavo59 <gustavosbarros59@gmail.com>
2021-11-21 14:52:20 -0300

    Criando estrutura inicial do projeto novamente

--- src/api/controllers/greeting.ts
+++
@@ -1,9 +0,0 @@
-import * as express from 'express'
-
-export function hello(req: express.Request, res: express.Response): void {
-    const name = req.query.name || 'stranger'
-    const message = `Hello, ${name}!`
-    res.json({
-        "message": message
-    })
-}
